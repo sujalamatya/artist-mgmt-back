7cb75c2fae02cee89609032a37580d54d05bb598
sujalamatya <sujalcr10@gmail.com>
2025-03-25 15:28:51 +0545

    fix: music and artist creation permission fix

--- artist/urls.py
+++ artist/urls.py
@@ -1,7 +1,9 @@
+
 from django.urls import path
 from .views import ArtistListView, ArtistDetailView
 
 urlpatterns = [
-    path('artists/', ArtistListView.as_view(), name='artist-list'),
-    path('artists/<int:artist_id>/', ArtistDetailView.as_view(), name='artist-detail'),
+    path("artists/", ArtistListView.as_view(), name="artist_list"),
+    path("artists/<int:artist_id>/", ArtistDetailView.as_view(), name="artist_detail"),
 ]
+
