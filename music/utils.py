81449170f56915ed339cbe40a81329c03b98206d
sujalamatya <sujalcr10@gmail.com>
2025-03-26 09:37:34 +0545

    feat: code refactor

---
+++ music/utils.py
@@ -0,0 +1,4 @@
+# def dictfetchall(cursor):
+#     """Return all rows from a cursor as a list of dictionaries."""
+#     columns = [col[0] for col in cursor.description]
+#     return [dict(zip(columns, row)) for row in cursor.fetchall()]
