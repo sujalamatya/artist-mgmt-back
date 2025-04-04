ed8d0ac92162451c50ace8d0cf76635c267a3367
sujalamatya <sujalcr10@gmail.com>
2025-04-04 09:13:00 +0545

    chore: other

--- events/serializers.py
+++ events/serializers.py
@@ -6,7 +6,6 @@ class EventSerializer(serializers.Serializer):
     user_id = serializers.IntegerField(allow_null=True, required=False)
     title = serializers.CharField(max_length=255)
     description = serializers.CharField(allow_blank=True, required=False)
-    event_date = serializers.DateTimeField()
+    event_date = serializers.DateField()
     location = serializers.CharField(max_length=255, allow_blank=True, required=False)
     created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
